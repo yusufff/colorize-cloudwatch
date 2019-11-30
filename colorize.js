3787768478e4c0e739b32016e7c24052d7fa09d8
Leo Barcellos <contato@leobarcellos.com>
2019-11-30 16:23:14 -0300

    Updating classes to new cloudwatch design

--- colorize.js
+++ colorize.js
@@ -4,37 +4,38 @@
  * http://github.com/yusufff
  */
 
-const WarningColor = '#FFCC80',
-			InfoColor = '#81D4FA',
-			DebugColor = '#B0BEC5',
-			ErrorColor = '#EF9A9A';
+const WarningColor = "#FFCC80",
+  InfoColor = "#81D4FA",
+  DebugColor = "#B0BEC5",
+  ErrorColor = "#EF9A9A";
 
 colorize = () => {
-	const logGroups = document.querySelectorAll('.cwdb-log-viewer-table-row-group:not(.colored)');
+  const logMessages = document.querySelectorAll(
+    ".awsui-table-row:not(.colored)"
+  );
 
-	if ( logGroups.length === 0 ) return false;
+  if (logMessages.length === 0) return false;
 
-	logGroups.forEach(log => {
-		const logMessage = log.querySelector('.cwdb-log-viewer-table-column-message');
-		if ( logMessage ) {
-			const logMessageText = logMessage.innerText;
-			let color;
-			if ( logMessageText.indexOf('INFO') !== -1 ) {
-				color = InfoColor;
-			} else if ( logMessageText.indexOf('WARNING') !== -1 ) {
-				color = WarningColor;
-			} else if ( logMessageText.indexOf('ERROR') !== -1 ) {
-				color = ErrorColor;
-			} else if ( logMessageText.indexOf('DEBUG') !== -1 ) {
-				color = DebugColor;
-			}
+  logMessages.forEach(log => {
+    const logMessageText = log.innerText;
+    if (logMessageText) {
+      let color;
+      if (logMessageText.indexOf("INFO") !== -1) {
+        color = InfoColor;
+      } else if (logMessageText.indexOf("WARNING") !== -1) {
+        color = WarningColor;
+      } else if (logMessageText.indexOf("ERROR") !== -1) {
+        color = ErrorColor;
+      } else if (logMessageText.indexOf("DEBUG") !== -1) {
+        color = DebugColor;
+      }
 
-			log.classList.add('colored');
-			log.style.backgroundColor = color;
-		}
-	})
-}
+      log.classList.add("colored");
+      log.style.backgroundColor = color;
+    }
+  });
+};
 
 const colorizeInterval = setInterval(() => {
-	window.requestAnimationFrame(colorize);
-}, 1000)
+  window.requestAnimationFrame(colorize);
+}, 1000);
